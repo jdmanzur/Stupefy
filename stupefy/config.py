ff0398aac8578e1c674fe7c7a56209209f3ee65e
JD <jade.almeida@dcomp.sor.ufscar.br>
2019-10-21 21:11:23 -0300

    fixed gitignore

--- stupefy/config.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-#youtube constants for values and api keys
-YTCLIENT_SECRETS_FILE = ""   #user data 
-YTAPI_KEY = ""   #key to use youtube data API
-YT_DESKTOP_KEY = ""     #key to use youtube data API on desktop app
-#spotify constants for api keys and values
-SPCLIENT_ID=""    #user ID for API
-SPCLIENT_SECRET = ""    #user secret key
