a6d600b18ee7956eca7b9378fe5862904ea76ec1
Jade <jade.almeida@dcomp.sor.ufscar.br>
2019-11-25 13:00:36 -0300

    Update config.py

---
+++ stupefy/settings_config/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+#youtube constants for values and api keys
+YTCLIENT_SECRETS_FILE = "settings_config/client_secret.json"   #user data 
+YTAPI_KEY = "AIzaSyCkwKbt6WlZHSw7xV18xxwfkT-OZeDnwXc"   #key to use youtube data API
+YT_DESKTOP_KEY = "298365440172-ubm6a7nv005bd1pqbn96iavg1gjen6pj.apps.googleusercontent.com"     #key to use youtube data API on desktop app
+#spotify constants for api keys and values
+SPCLIENT_ID="cd23967850ec488ea054f39ba6ad5161"    #user ID for API
+SPCLIENT_SECRET = "5fe09923eda44e58a37d610fcc1d6bbe"    #user secret key
+
+SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
